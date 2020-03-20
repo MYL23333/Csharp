a58bd79002d5c900c4953b14a1e67d6aa8b0ca1e
mmmmm <1912204673@qq.com>
2020-03-20 10:43:59 +0800

    sixth

---
+++ GenericList/GenericList/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Data { get; set; }
+            public Node(T  t)
+            {
+                Next = null;
+                Data = t;
+            }
+        }
+      
+        public class GenericList<T>
+        {
+            private Node<T> head;
+            private Node<T> tail;
+            public GenericList()
+            {
+                tail = head = null;
+            }
+
+            public Node<T> Head
+            {
+                get => head;
+            }
+
+            public void Add(T t)
+            {
+                Node<T> n = new Node<T>(t);
+                if (tail == null)
+                {
+                    head = tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> t = this.head;
+            while(t!=null)
+            {
+                action(t.Data);
+                t = t.Next;
+            }
+        }
+        }
+
+        class Program
+        {
+        static void Main(string[] args)
+        {
+            // 整型List
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+          
+            intlist.ForEach((int m) => Console.Write(m));
+            //
+            int max = int.MinValue;
+            intlist.ForEach(m => { if (max < m)  max = m; });
+            Console.Write($"最大值为: { max}");
+            int min = int.MaxValue;
+            intlist.ForEach(m => { if (min > m) min = m; });
+            Console.Write($"最小值为：{min}");
+            int sum = 0;
+            intlist.ForEach(m => { sum += m; });
+            Console.Write($"总和为：{ sum}");
+           
+
+            }
+
+        }
+    }
---
+++ alarm clock/alarm clock/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alarm_clock
+{
+
+    public delegate void ClockAlarm(object sender, ClockEventArgs args);
+    public class ClockEventArgs
+    {
+        public int CurrentHour;
+        public int CurrentMinute;
+        public int CurrentSecond;
+        public int SetHour = 3;
+        public int SetMinute = 4;
+        public int SetSecond = 5;
+        
+        
+    }
+    public class Clock
+    {
+        public event ClockAlarm Tick;
+        public event ClockAlarm Alarm;
+        private DateTime AlarmTime;
+        
+       
+        public void SClick(int Hour, int Minute, int Second)
+        {
+            //System.Timers.Timer t = new System.Timers.Timer(1000);
+
+
+            Console.Write($"现在是{Hour}时{Minute}分{Second}秒");
+            ClockEventArgs args1 = new ClockEventArgs()
+
+            {
+                CurrentHour = Hour,
+                CurrentMinute = Minute,
+                CurrentSecond = Second
+
+        };
+
+            //触发Alarm事件
+                Alarm(this, args1);
+            
+        }
+        public void Click(int Hour, int Minute,int Second)
+        {
+            Console.Write($"现在是{Hour}时{Minute}分{Second}秒");
+            ClockEventArgs args = new ClockEventArgs()
+            {
+                CurrentHour = Hour,
+                CurrentMinute = Minute,
+                CurrentSecond = Second
+            };
+          
+                //触发Tick事件
+               while(true)
+            {
+                Tick(this, args);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+
+    }
+    public class Form
+    {
+        public Clock Tick1 = new Clock();
+        public Clock Alarm2 = new Clock();
+        public Form()
+        {
+            Tick1.Tick += new ClockAlarm(Clock_Tick);
+            Alarm2.Alarm += new ClockAlarm(Clock_Alarm);
+        }
+        void Clock_Tick(object sender, ClockEventArgs args)
+        {
+            Console.Write("闹钟正在走时！");
+        }
+        void Clock_Alarm(object sender, ClockEventArgs args)
+        {
+            if (args.CurrentHour == args.SetHour && args.CurrentMinute == args.SetMinute && args.CurrentSecond == args.SetSecond)
+            {
+
+                Console.Write("闹钟正在响铃！");
+            }
+            else Console.Write("闹钟未到响铃时刻!");
+        }
+
+        /*public class SForm
+        {
+            public Clock Tick2 = new Clock();
+            public Clock Alarm2 = new Clock();
+            public SForm()
+            {
+                Alarm2.Tick += new ClockAlarm(Clock_Alarm);
+            }
+            void Clock_Alarm(object sender, ClockEventArgs args)
+            {
+                if (args.CurrentHour == args.SetHour && args.CurrentMinute == args.SetMinute && args.CurrentSecond == args.SetSecond)
+                {
+
+                    Console.Write("闹钟正在响铃！");
+                }
+                else Console.Write("闹钟未到响铃时刻!");
+            }
+
+        }*/
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+        
+           /* Form form1 = new Form();
+            form1.Tick1.Click(3, 4, 5);*/
+            Form form2 = new Form();
+            form2.Alarm2.SClick(4, 4, 5);
+        }
+    }
+}
