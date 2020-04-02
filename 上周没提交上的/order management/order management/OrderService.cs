0b9c6cea120982da06d36ff72bf0ed57f488b6de
mmmmm <1912204673@qq.com>
2020-04-02 23:24:50 +0800

    eight

---
+++ 上周没提交上的/order management/order management/OrderService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace order_management
+{
+    class OrderService
+    {
+        public string Client { set; get; }
+        public string ProductName { set; get; }
+        public int OrderNumber { set; get; }
+        public void CreateOrder(int ordernumber, string productname, string client)
+        {
+
+            OrderNumber = ordernumber;
+            ProductName = productname;
+            Client = client;
+            Order newOrder = new Order();
+            orders.Add(newOrder);
+        }
+        private List<Order> orders = new List<Order>();
+       
+        List<OrderService> OrderLists = new List<OrderService>();
+        public void OrderInquiry()
+        {
+            Console.Write("你想根据什么来查询？a.OrderNumber b.ProductName c.Client");
+            switch (Console.ReadLine())
+            {
+                case "a":
+                    Console.Write("输入订单号：");
+                    var query = OrderLists.Where(s => s.OrderNumber == int.Parse(Console.ReadLine()));
+                    break;
+                case "b":
+                    Console.Write("输入商品名称：");
+                    var query2 = OrderLists.Where(s => s.ProductName == Console.ReadLine());
+                    break;
+                case "c":
+                    Console.Write("客户名");
+                    var query3 = OrderLists.Where(s => s.Client == Console.ReadLine());
+                    break;
+            }
+        }
+    }
+}
---
+++ 上周没提交上的/order management/order management/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace order_management
+{
+      class Program
+    {
+        static void Main(string[] args)
+        {
+
+            OrderService os = new OrderService();
+
+                Console.WriteLine("a.查询订单，b.创建订单，c.退出系统,你的选择是：");
+                string m = Console.ReadLine();
+                switch (m)
+                {
+                    case "a":
+                        os.OrderInquiry();
+                        break;
+                    case "b":
+                    Console.WriteLine("请输入订单的订单号，商品名称，客户名：");
+                    os.CreateOrder(Convert.ToInt32(Console.ReadLine()), Console.ReadLine(), Console.ReadLine());
+                        break;
+                    case "c":
+                        return;
+                    default:
+                        Console.WriteLine("输入不规范，请重新输入。");
+                        return;
+                }
+            }
+        }
+    }
